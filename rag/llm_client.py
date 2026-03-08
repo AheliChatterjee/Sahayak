7ccea83d21ad45ce975bc8f3c1c2b57b08431e0e
Aheli Chatterjee <chatterjeeaheli2005@gmail.com>
2026-03-08 16:44:00 +0530

    Model changed to Llama-3-8B

--- rag/llm_client.py
+++ rag/llm_client.py
@@ -1,18 +1,21 @@
-from huggingface_hub import InferenceClient
+from urllib import response
+from dotenv import load_dotenv
 import os
+from huggingface_hub import InferenceClient
+
+load_dotenv()
 
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 client = InferenceClient(
-    model="mistralai/Mistral-7B-Instruct-v0.2",
+    model="meta-llama/Meta-Llama-3-8B-Instruct",
     token=HF_TOKEN
 )
 
 def call_llm(prompt: str) -> str:
-    response = client.text_generation(
-        prompt,
-        max_new_tokens=400,
-        temperature=0.2
+    response = client.chat_completion(
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=400,
+        temperature=0.2,
     )
-
-    return response
+    return response.choices[0].message.content
